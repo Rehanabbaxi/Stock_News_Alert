a516b9b4a9f8d1942a6209bb97964394fd642cb1
Rehanabbaxi <rehan020345@gmail.com>
2025-08-01 22:17:03 +0500

    final code of main

--- main.py
+++ main.py
@@ -1,86 +1,71 @@
 import requests
 from dotenv import load_dotenv
 import os
-from datetime import  datetime , timedelta
+from twilio.rest import Client
+
 STOCK = "TSLA"
-COMPANY_NAME = "Tesla Inc"
+COMPANY_NAME = "Trump Inc "
+
 load_dotenv()
-
-# # STEP 1: Use https://www.alphavantage.co
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-
-today = datetime.now()
-yesterday  = today - timedelta(days=1)
-yesterday = str(yesterday.date())
-yesterday = yesterday + " 19:00:00"
-day_before_yesterday = today - timedelta(days=2)
-day_before_yesterday = str(day_before_yesterday.date())
-day_before_yesterday = day_before_yesterday + " 19:00:00"
-
-
-
 stock_api  =  os.getenv("STOCK_API")
-stock_url = "https://www.alphavantage.co/query?"
-parameters = {"function" : "TIME_SERIES_INTRADAY" ,
-              "symbol" : "IBM" ,
-              "interval" : "60min",
-              "apikey" : stock_api}
+stock_endpoint = "https://www.alphavantage.co/query?"
+news_api = os.getenv("NEWS_API")
+news_endpoint  = "https://newsapi.org/v2/top-headlines?"
+twilio_auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+twilio_sid = os.getenv("TWILIO_ACC_SID")
 
-response  = requests.get(url = stock_url , params=parameters)
+
+
+stock_parameters = {"function" : "TIME_SERIES_DAILY" ,
+              "symbol" : STOCK ,
+              "apikey" : stock_api,
+             }
+
+response  = requests.get(url = stock_endpoint , params=stock_parameters)
 data = response.json()
-# print(data)
+data = data["Time Series (Daily)"]
+data = [values for (keys , values) in data.items()]
+yesterday_data = data[0]
+yesterday_closing_price = yesterday_data["4. close"]
+print(f"yesterday closing price : {yesterday_closing_price}")
+daybefore_yesterday_data = data[1]
+daybefore_yesterday_closing_price = daybefore_yesterday_data["4. close"]
+print(f"day before yestaerday closing price : {daybefore_yesterday_closing_price}")
 
-yesterday_data  = data["Time Series (60min)"][yesterday]
-day_before_yesterday_data = data["Time Series (60min)"][day_before_yesterday]
+difference = float(yesterday_closing_price) - float(daybefore_yesterday_closing_price)
+up_down = None
+if difference < 0 :
+    up_down = "⬆️"
+else:
+    up_down = "⬇️"
 
-print("day before yesterday points "+day_before_yesterday_data["4. close"])
-print("\n\n")
-print("yesterday points "+yesterday_data["4. close"])
-
-yesterday_closinig = float(yesterday_data["4. close"])
-day_before_yesterday_closing = float(day_before_yesterday_data["4. close"])
-difference  = day_before_yesterday_closing - yesterday_closinig
-difference =  difference
-percentage = (difference / day_before_yesterday_closing) * 100
-print(f"difference between points : {difference} which {percentage}%")
-
-
-
-## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
-
-#
-# news_api = os.getenv("NEWS_API")
-# url= "https://newsapi.org/v2/top-headlines?"
-# current_date = datetime.now().date()
-# parameters = {"q" : "trump",
-#             "country" : "us" ,
-#             "sortBy": "relevancy",
-#             "from": current_date,
-#             "to" : current_date,
-#             "apiKey" : news_api,
-#             "language" : "en"
-#             }
-# response = requests.get(url=url , params=parameters )
-# # response.raise_for_status()
-# data = response.json()
-# # print(len(data))
-# title = data["articles"][2]["title"]
-# description = data["articles"][2]["description"]
+difference_perc = round((difference / float(yesterday_closing_price)) * 100)
 
 
-## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number.
 
+if abs(difference_perc) > 2 :
+    news_parameters = {"q" : COMPANY_NAME,
+                      # "country" : "us" ,
+                      # "sortBy": "relevancy",
+                      # "from": current_date,
+                      # "to" : current_date,
+                      "apiKey": news_api,
+                      # "language": "en"
+                      }
+    response = requests.get(url=news_endpoint, params=news_parameters)
+    data = response.json()
+    articles = data["articles"]
+    three_articles = articles[:3]
+    # print(three_articles)
 
-#Optional: Format the SMS message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+    formatted_articles = [f"{STOCK} {difference_perc}% {up_down}\nHeadline: {article['title']} . \nBrief : {article['description']}" for article in three_articles]
+    client = Client(twilio_sid , twilio_auth_token)
+    for article in formatted_articles:
+        message =  client.messages.create(
+            body=article,
+            from_="+14708767821",
+            to="+92 315 5504532",
+        )
+        print(message.status)
+
 
